63b52b45bc34078315f26ce1919d9c7b2f242534
Ana Lívia Franco <analivia@live.com>
2020-10-31 14:22:01 -0300

    Autenticação de usuário e token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,48 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require("")
 
 const User = require('../models/User');
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
+
 router.post('/register', async(req, red) => {
   const { email } = req.body;
   try{
     if(await User.findOne({ email }))
-      return res.status(400).send({ error: 'User already exists' });
+      return res.status(400).send({ error: 'Usuário já cadastrado' });
 
     const user =  await User.creat(req.body);
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (err){
-    return res.status(400).send({ error: 'Registration failed '});
+    return res.status(400).send({ error: 'Falha no cadastro'});
   }
+});
+
+router.post('/authenticate', async(req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select('+password');
+
+  if(!user)
+    return res.status(400).send({ error: 'Usuário não encontrado'});
+
+  if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Senha inválida' });
+
+  user.password = undefined;
+
+  res.send({ user, token: generateToken({ id: user.id}), });
 });
 
 module.exports = app => app.use('/auth', router);
